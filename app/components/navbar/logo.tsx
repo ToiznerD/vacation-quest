d8866a940bfd24bbd89c44a3305012636c817035
Dor Toizner <dor@neurokaire.com>
2024-09-21 11:30:40 +0300

    Add logo for mobile

--- app/components/navbar/logo.tsx
+++ app/components/navbar/logo.tsx
@@ -7,14 +7,24 @@ const Logo = () => {
     const router = useRouter();
 
     return (
-        <Image
-            onClick={() => router.push('/')}
-            alt="Logo"
-            className="hidden md:block cursor-pointer"
-            height="150"
-            width="300"
-            src="/vacation-quest-logo.png"
-        />
+        <>
+            <Image
+                onClick={() => router.push('/')}
+                alt="Logo"
+                className="hidden md:block cursor-pointer"
+                height="150"
+                width="300"
+                src="/vacation-quest-logo.png"
+            />
+            <Image
+                onClick={() => router.push('/')}
+                alt="Logo"
+                className="block md:hidden cursor-pointer"
+                height="80"
+                width="80"
+                src="/logo.png"
+            />
+        </>
     )
 }
 
