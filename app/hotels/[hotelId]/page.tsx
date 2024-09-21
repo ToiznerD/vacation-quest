0e5a66149c975228697e4465e2d639a58e7367d9
Dor Toizner <dor@neurokaire.com>
2024-09-21 11:41:30 +0300

    Change +photos label

--- app/hotels/[hotelId]/page.tsx
+++ app/hotels/[hotelId]/page.tsx
@@ -254,7 +254,7 @@ const HotelPage = () => {
                             <div className="w-1/3 relative inline-block bg-black cursor-pointer h-40" onClick={galleryModal.onOpen}>
                                 <Image src={hotelInfo?.gallery.images[4].dynamic || ''} alt="{`Hotel image 0`}" layout="fill" objectFit="cover"
                                 className="object-cover relative z-1 opacity-60"/>
-                                <div className="absolute inset-0 flex items-center justify-center text-white text-xl font-bold z-20">
+                                <div className="absolute inset-0 flex items-center justify-center text-white text-base font-bold z-20">
                                     +{hotelInfo?.gallery.images.length || 0 - 5} Photos
                                 </div>
                             </div>
